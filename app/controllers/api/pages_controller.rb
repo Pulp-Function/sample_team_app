02c2084b696d695b1b8d1b81b28d33500282c5a7
Owen Luu <owenluu111@gmail.com>
2021-03-07 11:46:43 -0600

    test

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,5 +1,9 @@
 class Api::PagesController < ApplicationController
   def index
-    render json: { message: "Joey and Owen sux says hello!" }
+    render json: { message: "Joey and Owen do not sux and don't says hello!" }
+  end
+
+  def show
+    render json: { message: "Show for #{params[:id]}" }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,10 @@ Rails.application.routes.draw do
   # EXAMPLE JSON ROUTE WITH API NAMESPACE
   namespace :api do
     get "/pages" => "pages#index"
+<<<<<<< HEAD
     get "/pagess" => "pagess#index"
+=======
+    get "/pages/:id" => "pages#show"
+>>>>>>> a05f9405d6e3f2bbbcdd74c9de1b4508ce9cb833
   end
 end
