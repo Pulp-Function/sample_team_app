3b18e503acd6d8499b12c99f87bbf57d73e36aa4
Peter Jang <peter.x.jang@gmail.com>
2021-03-07 11:02:16 -0600

    Add show action

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -2,4 +2,8 @@ class Api::PagesController < ApplicationController
   def index
     render json: { message: "Joey and Owen sux says hello!" }
   end
+
+  def show
+    render json: { message: "Show for #{params[:id]}" }
+  end
 end
