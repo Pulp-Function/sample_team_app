152886a6dd642199b89f50966b1aa732af104a1b
Peter Jang <peter.x.jang@gmail.com>
2021-03-07 10:24:25 -0600

    Add second message

--- app/controllers/api/pages_controller.rb
+++ app/controllers/api/pages_controller.rb
@@ -1,5 +1,5 @@
 class Api::PagesController < ApplicationController
   def index
-    render json: { message: "Peter says hello!" }
+    render json: { message: "Peter says hello!", message2: "This is cool" }
   end
 end
